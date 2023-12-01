3ea0c0f5c1a8e94157086a1423a25bbcd3da1623
Abi <abisatti18@gmail.com>
2023-12-01 11:46:49 -0500

    first commit

---
+++ public/script.js
@@ -0,0 +1,219 @@
+const getCars = async () => {
+    try {
+        return (await fetch("/api/cars")).json();
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+const showCars = async () => {
+    let cars = await getCars();
+    let carsDiv = document.getElementById("car-list");
+    carsDiv.classList.add("flex-container");
+    carsDiv.classList.add("wrap");
+    carsDiv.innerHTML = "";
+
+    cars.forEach((car) => {
+        const section = document.createElement("section");
+        section.classList.add("car-model");
+        carsDiv.append(section);
+
+        const a = document.createElement("a");
+        a.href = "#";
+        section.append(a);
+
+        const h3 = document.createElement("h3");
+        h3.innerHTML = car.brand + " " + car.model;
+        a.append(h3);
+
+        a.onclick = (e) => {
+            e.preventDefault();
+            document.getElementById("hide-details").classList.remove("hidden");
+            displayDetails(car);
+        };
+
+        const editButton = document.createElement("button");
+        editButton.innerHTML = "Edit";
+        section.append(editButton);
+
+        editButton.onclick = (e) => {
+            e.preventDefault();
+            document.querySelector(".dialog").classList.remove("transparent");
+            document.getElementById("add-edit").innerHTML = "Edit Car Details";
+            populateEditForm(car);
+        };
+
+        const deleteButton = document.createElement("button");
+        deleteButton.innerHTML = "Delete";
+        section.append(deleteButton);
+
+        deleteButton.onclick = async (e) => {
+            e.preventDefault();
+            const confirmation = window.confirm("Are you sure you want to delete this car?");
+            if (confirmation) {
+                await deleteCar(car._id);
+            }
+        };
+    });
+};
+
+const displayDetails = (car) => {
+    const carDetails = document.getElementById("car-details");
+    carDetails.innerHTML = "";
+    carDetails.classList.add("flex-container");
+
+    const h3 = document.createElement("h3");
+    h3.innerHTML = car.brand + " " + car.model;
+    carDetails.append(h3);
+    h3.classList.add("pad-this");
+
+    const p1 = document.createElement("p");
+    carDetails.append(p1);
+    p1.innerHTML = 'Year: ' + car.year;
+    p1.classList.add("pad-this");
+
+    const p2 = document.createElement("p");
+    carDetails.append(p2);
+    p2.innerHTML = 'Type: ' + car.type;
+    p2.classList.add("pad-this");
+
+    const ul = document.createElement("ul");
+    carDetails.append(ul);
+    ul.classList.add("pad-this");
+
+    car.features.forEach((feature) => {
+        const li = document.createElement("li");
+        ul.append(li);
+        li.innerHTML = feature;
+    });
+
+    const editButton = document.createElement("button");
+    editButton.innerHTML = "Edit";
+    carDetails.append(editButton);
+
+    editButton.onclick = (e) => {
+        e.preventDefault();
+        document.querySelector(".dialog").classList.remove("transparent");
+        document.getElementById("add-edit").innerHTML = "Edit Car Details";
+        populateEditForm(car);
+    };
+};
+
+const populateEditForm = (car) => {
+    const form = document.getElementById("car-form");
+    form._id.value = car._id;
+    form.brand.value = car.brand;
+    form.model.value = car.model;
+    form.year.value = car.year;
+    form.type.value = car.type;
+
+    document.getElementById("feature-boxes").innerHTML = "";
+
+    car.features.forEach((feature) => {
+        const input = document.createElement("input");
+        input.type = "text";
+        input.value = feature;
+        document.getElementById("feature-boxes").appendChild(input);
+    });
+};
+
+const addCar = async (e) => {
+    e.preventDefault();
+    const form = document.getElementById("car-form");
+    const formData = new FormData(form);
+    const dataStatus = document.getElementById("data-status");
+    let response;
+
+    if (form._id.value == -1) {
+        formData.delete("_id");
+        formData.append("features", getFeatures());
+
+        response = await fetch("/api/cars", {
+            method: "POST",
+            body: formData
+        });
+    } else {
+        response = await fetch(`/api/cars/${form._id.value}`, {
+            method: "PUT",
+            body: formData
+        });
+    }
+
+    if (response.status !== 200) {
+        dataStatus.classList.remove("hidden");
+        dataStatus.innerHTML = "Error Posting Data!";
+        setTimeout(() => {
+            dataStatus.classList.add("hidden");
+        }, 3000);
+        console.error("Error posting data");
+        return;
+    }
+
+    response = await response.json();
+    resetForm();
+    document.querySelector(".dialog").classList.add("transparent");
+    showCars();
+};
+
+const deleteCar = async (carId) => {
+    try {
+        const response = await fetch(`/api/cars/${carId}`, {
+            method: "DELETE"
+        });
+
+        if (response.status !== 200) {
+            console.error("Error deleting car");
+            return;
+        }
+
+        const updatedCars = await response.json();
+        showCars(updatedCars);
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+const getFeatures = () => {
+    const inputs = document.querySelectorAll("#feature-boxes input");
+    let features = [];
+
+    inputs.forEach((input) => {
+        features.push(input.value);
+    });
+
+    return features;
+}
+
+const resetForm = () => {
+    const form = document.getElementById("car-form");
+    form.reset();
+    form._id = "-1";
+    document.getElementById("feature-boxes").innerHTML = "";
+};
+
+const showHideAdd = (e) => {
+    e.preventDefault();
+    document.querySelector(".dialog").classList.remove("transparent");
+    document.getElementById("add-edit").innerHTML = "Add Car";
+    resetForm();
+};
+
+const addFeature = (e) => {
+    e.preventDefault();
+    const section = document.getElementById("feature-boxes");
+    const input = document.createElement("input");
+    input.type = "text";
+    section.append(input);
+}
+
+window.onload = () => {
+    showCars();
+    document.getElementById("car-form").onsubmit = addCar;
+    document.getElementById("add-link").onclick = showHideAdd;
+
+    document.querySelector(".close").onclick = () => {
+        document.querySelector(".dialog").classList.add("transparent");
+    };
+
+    document.getElementById("add-feature").onclick = addFeature;
+};
---
+++ server.js
@@ -0,0 +1,182 @@
+const express = require("express");
+const app = express();
+const Joi = require("joi");
+const multer = require("multer");
+app.use(express.static("public"));
+app.use(express.json());
+const cors = require("cors");
+app.use(cors());
+
+const upload = multer({ dest: __dirname + "/public/images" });
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+let carModels = [
+    {
+        _id: 1,
+        brand: "Tesla",
+        model: "Model S",
+        year: 2023,
+        type: "Electric",
+        features: [
+            "Autopilot",
+            "Long Range",
+            "Fast Charging",
+            "Premium Interior",
+            "Performance Upgrade"
+        ]
+    },
+    {
+        _id: 2,
+        brand: "Toyota",
+        model: "Camry",
+        year: 2023,
+        type: "Sedan",
+        features: [
+            "Fuel Efficiency",
+            "Advanced Safety",
+            "Comfortable Interior",
+            "Smartphone Integration"
+        ]
+    },
+    {
+        "_id": 3,
+        "brand": "BMW",
+        "model": "X5",
+        "year": 2023,
+        "type": "SUV",
+        "features": [
+            "Luxurious Interior",
+            "Powerful Engine",
+            "Advanced Driver Assistance",
+            "Large Cargo Space",
+            "Panoramic Sunroof"
+        ]
+    },
+    {
+        "_id": 4,
+        "brand": "Honda",
+        "model": "Civic",
+        "year": 2023,
+        "type": "Compact",
+        "features": [
+            "Fuel Efficiency",
+            "Responsive Handling",
+            "Modern Infotainment",
+            "Honda Sensing Safety Suite"
+        ]
+    },
+    {
+        "_id": 5,
+        "brand": "Ford",
+        "model": "Mustang",
+        "year": 2023,
+        "type": "Sports Car",
+        "features": [
+            "Muscular Design",
+            "High-Performance Engine",
+            "Track-Focused Suspension",
+            "Brembo Brake System",
+            "SYNC 4 Infotainment"
+        ]
+    },
+    {
+        "_id": 6,
+        "brand": "Mercedes-Benz",
+        "model": "E-Class",
+        "year": 2023,
+        "type": "Luxury Sedan",
+        "features": [
+            "Premium Leather Interior",
+            "MBUX Infotainment System",
+            "Elegant Design",
+            "Smooth Ride",
+            "Driver Assistance Package"
+        ]
+    }
+];
+
+app.get("/api/cars", (req, res) => {
+    res.send(carModels);
+});
+
+app.post("/api/cars", upload.single("img"), (req, res) => {
+    const result = validateCar(req.body);
+
+    if (result.error) {
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+
+    const car = {
+        _id: carModels.length + 1,
+        brand: req.body.brand,
+        model: req.body.model,
+        year: req.body.year,
+        type: req.body.type,
+        features: req.body.features.split(","),
+    }
+
+    carModels.push(car);
+    res.send(carModels);
+});
+
+app.put("/api/cars/:id", upload.single("img"), (req, res) => {
+    const carId = parseInt(req.params.id);
+    const carIndex = carModels.findIndex(car => car._id === carId);
+
+    if (carIndex === -1) {
+        res.status(404).send("Car not found");
+        return;
+    }
+
+    const result = validateCar(req.body);
+
+    if (result.error) {
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+
+    carModels[carIndex] = {
+        _id: carId,
+        brand: req.body.brand,
+        model: req.body.model,
+        year: req.body.year,
+        type: req.body.type,
+        features: req.body.features.split(","),
+    };
+
+    res.send(carModels);
+});
+
+app.delete("/api/cars/:id", (req, res) => {
+    const carId = parseInt(req.params.id);
+    const carIndex = carModels.findIndex(car => car._id === carId);
+
+    if (carIndex === -1) {
+        res.status(404).send("Car not found");
+        return;
+    }
+
+    carModels.splice(carIndex, 1);
+    res.send(carModels);
+});
+
+const validateCar = (car) => {
+    const schema = Joi.object({
+        _id: Joi.optional(),
+        brand: Joi.string().min(1).required(),
+        model: Joi.string().required(),
+        year: Joi.number().integer().min(1900).required(),
+        type: Joi.string().required(),
+        features: Joi.allow(""),
+    });
+
+    return schema.validate(car);
+};
+
+app.listen(3000, () => {
+    console.log("Running");
+});
